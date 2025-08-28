3895474c121050f35ff0e9e9f5764a30b768b9ed
Dominik Ottenbreit <dom@newmatik.com>
2025-08-28 09:20:03 +0200

    restructure: move site into website/ with website/docs/, consolidate assets, fix relative paths and 404 routing; remove stray root assets

--- website/assets/site.js
+++ website/assets/site.js
@@ -10,14 +10,14 @@
   }
 
   function isDocs() {
-    // When hosted from /docs/ folder, everything is the docs app
-    return true;
+    const p = location.pathname.replace(/\/index.html$/, '');
+    return /\/docs\/?$/.test(p) || /\/docs\//.test(p);
   }
 
   function header() {
     const here = isDocs() ? 'docs' : 'home';
-    const homeHref = './';
-    const docsHref = './';
+    const homeHref = '../'.repeat((location.pathname.match(/\//g) || []).length - (isDocs() ? 2 : 1)) || './';
+    const docsHref = homeHref + 'docs/';
     const githubHref = 'https://github.com/newmatik/edgesentinel-web';
     return el(`
       <header class="header">
@@ -42,11 +42,11 @@
         <div class="container">
           <div>© ${year} EdgeSentinel</div>
           <div>
-            <a href="./">Docs</a>
+            <a href="${docsHref}">Docs</a>
             &nbsp;•&nbsp;
             <a href="https://github.com/newmatik/edgesentinel-web" target="_blank" rel="noopener">Open Source</a>
             &nbsp;•&nbsp;
-            <a href="./imprint.html">Imprint</a>
+            <a href="${homeHref}imprint.html">Imprint</a>
           </div>
         </div>
       </footer>
